cae4f79f5e4a4ef7a67c20130bf2d6ec5128f1cf
Ville Leppänen <ville.leppanen@heeros.com>
2020-06-26 13:39:09 +0300

    lints

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-bar">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-bar'>
+        <img src={logo} className='App-logo' alt='logo' />
       </header>
-      <main className="App-content">
+      <main className='App-content'>
         content
       </main>
-      <footer className="App-footer">
+      <footer className='App-footer'>
         footer
       </footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
